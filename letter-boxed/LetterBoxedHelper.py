68f5d5ce00083110ee43889f757997ca995bca79
zoomjuice <78334783+zoomjuice@users.noreply.github.com>
2024-09-17 12:28:43 -0400

    Standardize import naming conventions

--- letter-boxed/LetterBoxedHelper.py
+++ letter-boxed/LetterBoxedHelper.py
@@ -1,21 +1,21 @@
-import LetterBoxedBase as lb
+import LetterBoxedBase as lbb
 from base import scraper
 
 date_str = scraper.get_today_date()
-game_data = scraper.load_puzzle_data(lb.game_name, date_str)
+game_data = scraper.load_puzzle_data(lbb.game_name, date_str)
 
 # Get letters for current puzzle
-letter_set = lb.extract_letters(game_data)
+letter_set = lbb.extract_letters(game_data)
 
 # Load user's word list
 user_word_file = 'my_words.txt'
-user_word_list = lb.load_user_words(user_word_file)
+user_word_list = lbb.load_user_words(user_word_file)
 
 solutions = [
-    lb.one_word_solve(user_word_list, letter_set),
-    lb.two_word_solve(user_word_list, letter_set),
-    lb.three_word_solve(user_word_list, letter_set)
+    lbb.one_word_solve(user_word_list, letter_set),
+    lbb.two_word_solve(user_word_list, letter_set),
+    lbb.three_word_solve(user_word_list, letter_set)
     ]
 
 for solution_list in solutions:
-    lb.print_solutions(solution_list)
+    lbb.print_solutions(solution_list)
--- letter-boxed/LetterBoxedSolver.py
+++ letter-boxed/LetterBoxedSolver.py
@@ -1,21 +1,21 @@
-import LetterBoxedBase as lb
+import LetterBoxedBase as lbb
 from base import scraper
 
 date_str = scraper.get_today_date()
-game_data = scraper.load_puzzle_data(lb.game_name, date_str)
+game_data = scraper.load_puzzle_data(lbb.game_name, date_str)
 
 # Get letters, word list, and chosen solution for current puzzle
-letter_set = lb.extract_letters(game_data)
-word_list = lb.extract_word_list(game_data)
+letter_set = lbb.extract_letters(game_data)
+word_list = lbb.extract_word_list(game_data)
 nyt_solution = '-'.join(game_data['ourSolution']).lower()
 
 solutions = [
-    lb.one_word_solve(word_list, letter_set),
-    lb.two_word_solve(word_list, letter_set),
-    lb.three_word_solve(word_list, letter_set)
+    lbb.one_word_solve(word_list, letter_set),
+    lbb.two_word_solve(word_list, letter_set),
+    lbb.three_word_solve(word_list, letter_set)
     ]
 
 for solution_list in solutions:
-    lb.print_solutions(solution_list)
+    lbb.print_solutions(solution_list)
 
 print('\nNYT\'s solution:\n' + nyt_solution)
